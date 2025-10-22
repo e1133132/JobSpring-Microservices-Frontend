eec5ba64891d3c9d6031565cbd7bb1eb142909b1
e1133132 <e1133132@u.nus.edu>
2025-10-22 23:27:55 +0800

    fix bugs

--- src/components/hr/applicationDetails.jsx
+++ src/components/hr/applicationDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import Navigation from "../navigation.jsx";
 import { FaArrowLeft } from "react-icons/fa";
